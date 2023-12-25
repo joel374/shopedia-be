b0c81b082250f49e8349f86bac7564c9a08731f7
Joel <joellegifanimanisali@gmail.com>
2023-12-25 05:39:35 +0000

    Change api config

--- config/connectionConfig.js
+++ config/connectionConfig.js
@@ -3,11 +3,11 @@ const config = require('./config.json')
 
 const pool = mysql.createPool({
     connectionLimit: 100,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: config.development.host,
+    user: config.development.username,
+    password: config.development.password,
+    database: config.development.database,
+    port: '3306'
 });
 
 module.exports = pool;
